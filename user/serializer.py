808ba3cc8f401e5aa93e2adcb4e68a85a14d1c28
benjamin <benjamin.huete.n@gmail.com>
2023-08-21 17:01:10 -0400

    cambios en serializador

--- user/serializer.py
+++ user/serializer.py
@@ -24,8 +24,32 @@ class UserRegisterationSerializer(serializers.ModelSerializer):
         fields = ("id", "username", "email", "password")
         extra_kwargs = {"password": {"write_only": True}}
 
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError("The given username must be set")
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        return self.create_user(**validated_data)
 
 
 class UserLoginSerializer(serializers.Serializer):
