b93134175bbcf0a586075894d332f07ab970a630
Adrian <adrian.herrera7108@gmail.com>
2023-08-24 21:19:15 -0400

    prueba rutas

--- institution/views.py
+++ institution/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
 from .serializer import InstitutionsSerializer
 from .models import Institutions
-
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
 class InstitutionViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
     queryset = Institutions.objects.all()
     serializer_class = InstitutionsSerializer
